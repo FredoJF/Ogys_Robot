3202ce7d040aef27e1a6ff7e3942984ad9400826
Ogysap <frederic.annequin@gmail.com>
2022-03-10 11:58:23 -0500

    Change floor by trunc

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,7 +3,7 @@ module.exports = {
   aliases: ["cf"],
 
   action(msg) {
-    var x = Math.floor(Math.random() * 2);
+    var x = Math.trunc(Math.random() * 2);
     if (x == 0) msg.reply("Face.");
     else msg.reply("Pile.");
   },
